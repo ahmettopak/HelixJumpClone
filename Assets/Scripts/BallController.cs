61b7b69097a6585a2a6212a954b0b335975e307c
Ahmet TOPAK <75997899+ahmettopak@users.noreply.github.com>
2023-01-31 11:41:10 +0300

    Level system create

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -7,28 +7,31 @@ public class BallController : MonoBehaviour
 {
 
     private Rigidbody rigidbody;
-    //[SerializeField] private GameObject splashImg;
+    [SerializeField] private GameObject splashImg;
     [SerializeField] private float jumpForce;
     private GameManager gameManager;
     private RingController ring;
 
     void Start() {
-        rigidbody= GetComponent<Rigidbody>();
+        rigidbody = GetComponent<Rigidbody>();
         gameManager = GameObject.FindObjectOfType<GameManager>();
     }
 
     private void OnCollisionEnter(Collision other) {
         rigidbody.AddForce(Vector3.up * jumpForce);
-        //GameObject splash = Instantiate(splashImg, transform.position - new Vector3(0, 0.22f, 0f), transform.rotation);
-        // splash.transform.SetParent(other.gameObject.transform);
-        //Destroy(splash, 1);
+        GameObject splash = Instantiate(splashImg, transform.position - new Vector3(0, 0.1f, 0),splashImg.transform.rotation);
+        splash.transform.SetParent(other.gameObject.transform);
+        Destroy(splash, 1);
 
-        
+
         if (other.gameObject.tag=="UnSafeArea") {
-            Debug.Log("Rest");
-            gameManager.restartGame();
+            Time.timeScale = 0;
+            gameManager.setPanel("gameOverPanel",true);
+            
         }
         else if (other.gameObject.tag=="FinishArea") {
+            Time.timeScale = 0;
+            gameManager.nextLevel();
             Debug.Log("Finish");
 
         }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -8,17 +8,47 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     public int score;
-   // public Text scoreText;
-    public TextMeshProUGUI scoreText;
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private GameObject gameOverPanel;
+    [SerializeField] private GameObject mainMenuPanel;
     public void Start() {
+        Time.timeScale = 0;
         gameScore(0);
+        if (PlayerPrefs.GetInt("playerLevel") == null) {
+            PlayerPrefs.SetInt("playerLevel",1);
+            Debug.Log(PlayerPrefs.GetInt("playerLevel"));
+
+        }
+        gameOverPanel.SetActive(false);
+      
     }
     public void gameScore(int ringScore) {
         score += ringScore;
         scoreText.text = score.ToString();
     }
-
+    public void playGame() {
+        Time.timeScale =1;
+        gameOverPanel.SetActive(false);
+        mainMenuPanel.SetActive(false);
+    }
     public void restartGame() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        
+    }
+    public void nextLevel() {
+        PlayerPrefs.SetInt("playerLevel", PlayerPrefs.GetInt("playerLevel")+1);
+        SceneManager.LoadScene(PlayerPrefs.GetInt("playerLevel"));
+    }
+    public void setPanel(string panelName,bool active) {
+        if (panelName=="gameOverPanel") {
+            gameOverPanel.SetActive(active);
+        }
+        else if (panelName=="mainMenuPanel") {
+            mainMenuPanel.SetActive(active);
+
+        }
+        else {
+            Debug.LogError("Yanlış değer girdiniz");
+        }
     }
 }
